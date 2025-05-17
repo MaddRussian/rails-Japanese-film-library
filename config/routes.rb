5f32a88dd229703c8aa9efd3144812dfb757e14c
William Sebastian <okeefesebastian91@gmail.com>
2025-05-17 15:42:17 +0900

    added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "lists#index"
-  resources :lists, only: [:index, :show, :create, :destroy] do
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
     resources :bookmarks, only: [:create]
   end
   resources :bookmarks, only: :destroy
