da3e39652e6241978a760c50385607c13cbf2f23
William Sebastian <okeefesebastian91@gmail.com>
2025-06-16 19:31:56 +0900

    Improve bookmark creation error handling and form submission

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,14 @@
 class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
+    Rails.logger.info "Bookmark params: #{params.inspect}"
     @bookmark = @list.bookmarks.new(bookmark_params)
+
     if @bookmark.save
       redirect_to list_path(@list), notice: "Film added to #{@list.name}"
     else
-      redirect_to root_path, alert: "Failed to add film."
+      Rails.logger.error "Bookmark creation failed: #{@bookmark.errors.full_messages.join(', ')}"
+      redirect_to list_path(@list), alert: "Failed to add film: #{@bookmark.errors.full_messages.join(', ')}"
     end
   end
 
